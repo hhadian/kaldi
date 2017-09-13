4318492799b38a449df9857518ff2a609738ba70
Chun-Chieh Chang <cchunch1@jhu.edu>
2017-09-13 17:27:45 -0400

    made changes to match ones made in aarora8/iam scripts located in egs/iam/s5/local

--- egs/uw3/v1/local/prepare_lm.sh
+++ egs/uw3/v1/local/prepare_lm.sh
@@ -1,37 +1,28 @@
 #!/bin/bash
 
-
-# Copyright 2015 Sharif University of Technology (Author: Hossein Hadian)
+# Copyright 2015     Hossein Hadian
 # Apache 2.0
 
-grammar_words=false
 
-[ -f ./path.sh ] && . ./path.sh; # source the path.
-. parse_options.sh || exit 1;
+ngram=2
 
-# this is only called for decoding purposes. This is not necessary for training.
-lmsrc=$1  # Can be either a text file in the format of train/text or arpa file
-lang=$2   # output dir for e.g. data/test/lang
-ngram=$3
-[ -z $ngram ] && ngram=2
+[ -f ./path.sh ] && . ./path.sh  # source the path.
+. parse_options.sh || exit 1
+
+# This is only called for decoding purposes. This is not necessary for training.
+if [ $# != 2 ]; then
+  echo "Usage: $0 [--ngram <n-gram-order>] <lm-src> <out-lang-dir>"
+  echo "<lm-src> can either be an ARPA file or transcriptions file, e.g. train/text"
+  exit 1;
+fi
+
+lmsrc=$1
+lang=$2
+
 tmpdir=$lang/temp
-test=$lang
-
-
 mkdir -p $tmpdir
 
-if $grammar_words; then
-  echo "-------------------- GRAMMAR LM -----------------------"
-  echo "-------------------- GRAMMAR LM -----------------------"
-  
-  sed "s| [[:digit:]]\+||g" $test/words.txt | grep -v "[<&(#s]"  >$tmpdir/just_words.txt
-  #python local/lex2G.py $tmpdir/just_words.txt >$tmpdir/pG.txt
-  awk '{print $2}' $lmsrc | python local/make_isolated_word_grammar.py >$tmpdir/pG.txt
-  fstcompile --isymbols=$test/words.txt --osymbols=$test/words.txt $tmpdir/pG.txt >$test/G.fst
-elif [[ $lmsrc == *".arpa" ]]; then
-  echo "-------------------- ARPA LM -----------------------"
-  echo "-------------------- ARPA LM -----------------------"
-
+if [[ $lmsrc == *.arpa ]]; then
   echo "$0: Using arpa LM: "$lmsrc
   arpa=$lmsrc
 else
@@ -41,18 +32,16 @@ else
     export PATH=$PWD/../../../tools/irstlm/bin:$PATH
     echo "$0: Assuming irstlm is at: "$IRSTLM
   fi
-#  [ -z "$IRSTLM" ] && \
-#    echo "LM building won't work without setting the IRSTLM env variable" && exit 1;
-  ! which build-lm.sh 2>/dev/null  && \
+  ! which build-lm.sh 2>/dev/null && \
     echo "$0: IRSTLM does not seem to be installed (build-lm.sh not on your path): " && \
     echo "go to <kaldi-root>/tools and try 'make irstlm_tgt'" && exit 1;
 
   cut -d' ' -f2- $lmsrc | sed -e 's:^:<s> :' -e 's:$: </s>:' \
-    > $tmpdir/tmp_lm_train
-  build-lm.sh -k 1 -i $tmpdir/tmp_lm_train -n $ngram -o $tmpdir/tmp_lm_bg.ilm.gz
+                              > $tmpdir/tmp_lm_train
+  build-lm.sh -k 1 -i $tmpdir/tmp_lm_train -n $ngram -o $tmpdir/tmp_lm.ilm.gz
 
-  compile-lm $tmpdir/tmp_lm_bg.ilm.gz -t=yes /dev/stdout | \
-  grep -v unk > $tmpdir/lm_phone_bg.arpa
+  compile-lm $tmpdir/tmp_lm.ilm.gz -t=yes /dev/stdout | \
+    grep -v unk > $tmpdir/lm_phone_bg.arpa
 
   arpa=$tmpdir/lm_phone_bg.arpa
 fi
@@ -65,8 +54,9 @@ cat $arpa | \
     grep -v '</s> </s>' | \
     arpa2fst - | fstprint | \
     utils/remove_oovs.pl $tmpdir/oovs.txt | \
-    utils/eps2disambig.pl | utils/s2eps.pl | fstcompile --isymbols=$lang/words.txt \
-      --osymbols=$lang/words.txt  --keep_isymbols=false --keep_osymbols=false | \
-     fstrmepsilon | fstarcsort --sort_type=ilabel >$lang/G.fst || exit 1;
+    utils/eps2disambig.pl | utils/s2eps.pl | \
+    fstcompile --isymbols=$lang/words.txt --osymbols=$lang/words.txt \
+               --keep_isymbols=false --keep_osymbols=false | \
+    fstrmepsilon | fstarcsort --sort_type=ilabel >$lang/G.fst || exit 1;
 
-echo "$0: Done preparing LM"
+echo "$0: Done preparing the LM"
