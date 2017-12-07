c1adb95902f93be0cd79414d8737219c5d353ad7
aarora8 <aarora8@jhu.edu>
2017-12-07 14:22:07 -0500

    minor change

--- egs/iam/s5/local/prepare_data.sh
+++ egs/iam/s5/local/prepare_data.sh
@@ -23,78 +23,78 @@ if [ -f path.sh ]; then . ./path.sh; fi
 
 #download dir
 add_val_data_train=false
-dl_dir=data/download
-lines=$dl_dir/lines
-xml=$dl_dir/xml
-ascii=$dl_dir/ascii
-bcorpus=$dl_dir/browncorpus
-lobcorpus=$dl_dir/lobcorpus
-data_split_info=$dl_dir/largeWriterIndependentTextLineRecognitionTask
+download_dir=data/download
+lines=$download_dir/lines
+xml=$download_dir/xml
+ascii=$download_dir/ascii
+bcorpus=$download_dir/browncorpus
+lobcorpus=$download_dir/lobcorpus
+data_split_info=$download_dir/largeWriterIndependentTextLineRecognitionTask
 lines_url=http://www.fki.inf.unibe.ch/DBs/iamDB/data/lines/lines.tgz
 xml_url=http://www.fki.inf.unibe.ch/DBs/iamDB/data/xml/xml.tgz
 data_split_info_url=http://www.fki.inf.unibe.ch/DBs/iamDB/tasks/largeWriterIndependentTextLineRecognitionTask.zip
 ascii_url=http://www.fki.inf.unibe.ch/DBs/iamDB/data/ascii/ascii.tgz
 brown_corpus_url=http://www.sls.hawaii.edu/bley-vroman/brown.txt
 lob_corpus_url=http://ota.ox.ac.uk/text/0167.zip
-mkdir -p $dl_dir
+mkdir -p $download_dir
 #download and extact images and transcription
 if [ -d $lines ]; then
   echo Not downloading lines images as it is already there.
 else
-  if [ ! -f $dl_dir/lines.tgz ]; then
+  if [ ! -f $download_dir/lines.tgz ]; then
     echo Downloading lines images...
-    wget -P $dl_dir --user userjh --password password $lines_url || exit 1;
+    wget -P $download_dir --user userjh --password password $lines_url || exit 1;
   fi
   mkdir -p $lines
-  tar -xzf $dl_dir/lines.tgz -C $lines || exit 1;
+  tar -xzf $download_dir/lines.tgz -C $lines || exit 1;
   echo Done downloading and extracting lines images
 fi
 
 if [ -d $xml ]; then
   echo Not downloading transcription as it is already there.
 else
-  if [ ! -f $dl_dir/xml.tgz ]; then
+  if [ ! -f $download_dir/xml.tgz ]; then
     echo Downloading transcription ...
-    wget -P $dl_dir --user userjh --password password $xml_url || exit 1;
+    wget -P $download_dir --user userjh --password password $xml_url || exit 1;
   fi
   mkdir -p $xml
-  tar -xzf $dl_dir/xml.tgz -C $xml || exit 1;
+  tar -xzf $download_dir/xml.tgz -C $xml || exit 1;
   echo Done downloading and extracting transcription
 fi
 
 if [ -d $data_split_info ]; then
   echo Not downloading data split, training and testing split, information as it is already there.
 else
-  if [ ! -f $dl_dir/largeWriterIndependentTextLineRecognitionTask.zip ]; then
+  if [ ! -f $download_dir/largeWriterIndependentTextLineRecognitionTask.zip ]; then
     echo Downloading training and testing data Split Information ...
-    wget -P $dl_dir --user userjh --password password $data_split_info_url || exit 1;
+    wget -P $download_dir --user userjh --password password $data_split_info_url || exit 1;
   fi
   mkdir -p $data_split_info
-  unzip $dl_dir/largeWriterIndependentTextLineRecognitionTask.zip -d $data_split_info || exit 1;
+  unzip $download_dir/largeWriterIndependentTextLineRecognitionTask.zip -d $data_split_info || exit 1;
   echo Done downloading and extracting training and testing data Split Information
 fi
 
 if [ -d $ascii ]; then
   echo Not downloading ascii folder as it is already there.
 else
-  if [ ! -f $dl_dir/ascii.tgz ]; then
+  if [ ! -f $download_dir/ascii.tgz ]; then
     echo Downloading ascii folder ...
-    wget -P $dl_dir --user userjh --password password $ascii_url || exit 1;
+    wget -P $download_dir --user userjh --password password $ascii_url || exit 1;
   fi
   mkdir -p $ascii
-  tar -xzf $dl_dir/ascii.tgz -C $ascii || exit 1;
+  tar -xzf $download_dir/ascii.tgz -C $ascii || exit 1;
   echo Done downloading and extracting ascii folder
 fi
 
 if [ -d $lobcorpus ]; then
   echo Not downloading lob corpus as it is already there.
 else
-  if [ ! -f $dl_dir/0167.zip ]; then
+  if [ ! -f $download_dir/0167.zip ]; then
     echo Downloading lob corpus ...
-    wget -P $dl_dir $lob_corpus_url || exit 1;
+    wget -P $download_dir $lob_corpus_url || exit 1;
   fi
   mkdir -p $lobcorpus
-  unzip $dl_dir/0167.zip -d $lobcorpus || exit 1;
+  unzip $download_dir/0167.zip -d $lobcorpus || exit 1;
   echo Done downloading and extracting lob corpus
 fi
 
@@ -115,10 +115,10 @@ testset=testset.txt
 trainset=trainset.txt
 val1=validationset1.txt
 val2=validationset2.txt
-test_path="$dl_dir/$file_name/$testset"
-train_path="$dl_dir/$file_name/$trainset"
-val1_path="$dl_dir/$file_name/$val1"
-val2_path="$dl_dir/$file_name/$val2"
+test_path="$download_dir/$file_name/$testset"
+train_path="$download_dir/$file_name/$trainset"
+val1_path="$download_dir/$file_name/$val1"
+val2_path="$download_dir/$file_name/$val2"
 
 new_train_set=new_trainset.txt
 new_test_set=new_testset.txt
@@ -138,9 +138,9 @@ else
 fi
 
 if [ $stage -le 0 ]; then
-  local/process_data.py $dl_dir data/train data --dataset new_trainset --model_type word || exit 1
-  local/process_data.py $dl_dir data/test data --dataset new_testset --model_type word || exit 1
-  local/process_data.py $dl_dir data/val data --dataset new_valset --model_type word || exit 1
+  local/process_data.py $download_dir data/train data --dataset new_trainset --model_type word || exit 1
+  local/process_data.py $download_dir data/test data --dataset new_testset --model_type word || exit 1
+  local/process_data.py $download_dir data/val data --dataset new_valset --model_type word || exit 1
 
   utils/utt2spk_to_spk2utt.pl data/train/utt2spk > data/train/spk2utt
   utils/utt2spk_to_spk2utt.pl data/test/utt2spk > data/test/spk2utt
